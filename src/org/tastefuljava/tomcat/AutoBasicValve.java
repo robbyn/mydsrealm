2ecc778d39cfc46bc58c537ed64044c86237ab41
maurice <maurice@192.168.1.104>
2015-10-24 14:16:49 +0200

    My

---
+++ src/org/tastefuljava/tomcat/AutoBasicValve.java
@@ -0,0 +1,55 @@
+package org.tastefuljava.tomcat;
+
+import java.io.IOException;
+import java.security.Principal;
+import javax.servlet.ServletException;
+import org.apache.catalina.Realm;
+import org.apache.catalina.connector.Request;
+import org.apache.catalina.connector.Response;
+import org.apache.catalina.valves.ValveBase;
+
+public class AutoBasicValve extends ValveBase {
+    private static final String BASIC_PREFIX = "basic ";
+
+    @Override
+    public void invoke(Request request, Response response)
+            throws IOException, ServletException {
+        Principal principal = request.getUserPrincipal();
+        Realm realm = getContainer().getRealm();
+        if (principal != null) {
+            if (containerLog.isDebugEnabled()) {
+                containerLog.debug(
+                        "Already authenticated as: " + principal.getName());
+            }
+        } else if (realm == null) {
+            if (containerLog.isDebugEnabled()) {
+                containerLog.debug("No realm configured");
+            }
+        } else {
+            String auth = request.getHeader("authorization");
+            if (auth != null) {
+                if (auth.toLowerCase().startsWith(BASIC_PREFIX)) {
+                    auth = auth.substring(BASIC_PREFIX.length());
+                    byte[] bytes = Base64.decode(auth);
+                    auth = new String(bytes, "UTF-8");
+                    int ix = auth.indexOf(':');
+                    if (ix >= 0) {
+                        String username = auth.substring(0, ix);
+                        String password = auth.substring(ix+1);
+                        principal = realm.authenticate(username, password);
+                        if (principal == null) {
+                            containerLog.warn(
+                                    "Could not authenticate " + username);
+                        } else {
+                            containerLog.info(
+                                    "Authenticated as " + principal.getName());
+                            request.setAuthType("BASIC");
+                            request.setUserPrincipal(principal);
+                        }
+                    }
+                }
+            }
+        }
+        getNext().invoke(request, response);
+    }
+}
